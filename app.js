e6332529cea294ad2347f76a926e2dda41b88bcb
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-01-20 11:12:53 +0600

    upload text mask

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const light=document.querySelector('.light')
-const dark=document.querySelector('.dark')
+const light = document.querySelector(".light");
+const dark = document.querySelector(".dark");
 
 // dark div moving animation
-dark.onmousemove=(e)=>{
-    const w=window.outerWidth
-    const h=window.outerHeight
-const x=e.clientX
-const y=e.clientY
-document.querySelector("h1").style.backgroundPosition = `${(x / w) * 100}%  ${
-  (2*y / h) * 100
-}%`;
-}
+dark.onmousemove = (e) => {
+  const w = window.outerWidth;
+  const h = window.outerHeight;
+  const x = e.clientX;
+  const y = e.clientY;
+  document.querySelector("h1").style.backgroundPosition = `${(x / w) * 100}%  ${
+    ((2 * y) / h) * 100
+  }%`;
+};
 // light div moving animation
-light.onmousemove=(e)=>{
-    const w=window.outerWidth
-    const h=window.outerHeight
-const x=e.clientX
-const y=e.clientY
-document.querySelector("h2").style.backgroundPosition = `${(x / w) * 100}%  ${
-  (y / h) * 100
-}%`;
-
-}
+light.onmousemove = (e) => {
+  const w = window.outerWidth;
+  const h = window.outerHeight;
+  const x = e.clientX;
+  const y = e.clientY;
+  document.querySelector("h2").style.backgroundPosition = `${(x / w) * 100}%  ${
+    (y / h) * 100
+  }%`;
+};
